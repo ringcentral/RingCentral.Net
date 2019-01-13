bbf01d281f278b96b40c9fe50d961898a9db0770
Tyler Long <tyler4long@gmail.com>
2019-01-13 21:11:52 +0800

    C# using()

--- RingCentral.Tests/SmsTest.cs
+++ RingCentral.Tests/SmsTest.cs
@@ -9,18 +9,18 @@ namespace RingCentral.Tests
         public async void TestSendSms()
         {
             var env = Environment.GetEnvironmentVariables();
-            var rc = new RestClient(
+            using (var rc = new RestClient(
                 env["RINGCENTRAL_CLIENT_ID"] as string,
                 env["RINGCENTRAL_CLIENT_SECRET"] as string,
                 env["RINGCENTRAL_WSG_URL"] as string
-            );
-            await rc.Authorize(
-                env["RINGCENTRAL_USERNAME"] as string,
-                env["RINGCENTRAL_EXTENSION"] as string,
-                env["RINGCENTRAL_PASSWORD"] as string
-            );
-
-            await rc.Revoke();
+            ))
+            {
+                await rc.Authorize(
+                    env["RINGCENTRAL_USERNAME"] as string,
+                    env["RINGCENTRAL_EXTENSION"] as string,
+                    env["RINGCENTRAL_PASSWORD"] as string
+                );
+            }
         }
     }
 }
--- RingCentral.Tests/SubscriptionTest.cs
+++ RingCentral.Tests/SubscriptionTest.cs
@@ -9,24 +9,25 @@ namespace RingCentral.Tests
         public async void TestSetupSubscription()
         {
             var env = Environment.GetEnvironmentVariables();
-            var rc = new RestClient(
+            using (var rc = new RestClient(
                 env["RINGCENTRAL_CLIENT_ID"] as string,
                 env["RINGCENTRAL_CLIENT_SECRET"] as string,
                 env["RINGCENTRAL_WSG_URL"] as string
-            );
-            await rc.Authorize(
-                env["RINGCENTRAL_USERNAME"] as string,
-                env["RINGCENTRAL_EXTENSION"] as string,
-                env["RINGCENTRAL_PASSWORD"] as string
-            );
-            var eventFilters = new string[] { "/restapi/v1.0/account/~/extension/~/presence?detailedTelephonyState=true" };
-            var r = await rc.Subscribe(eventFilters, msg =>
+            ))
             {
-                Console.WriteLine(msg);
-            });
-            Assert.Equal(200, r.metadata.status);
-            Assert.Equal("WebSocket", r.body.deliveryMode.transportType);
-            await rc.Revoke();
+                await rc.Authorize(
+                    env["RINGCENTRAL_USERNAME"] as string,
+                    env["RINGCENTRAL_EXTENSION"] as string,
+                    env["RINGCENTRAL_PASSWORD"] as string
+                );
+                var eventFilters = new string[] { "/restapi/v1.0/account/~/extension/~/presence?detailedTelephonyState=true" };
+                var r = await rc.Subscribe(eventFilters, msg =>
+                {
+                    Console.WriteLine(msg);
+                });
+                Assert.Equal(200, r.metadata.status);
+                Assert.Equal("WebSocket", r.body.deliveryMode.transportType);
+            }
         }
     }
 }
