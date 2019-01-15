4f93c8c1cb4664276370e3b14b127eeef27e00ca
Tyler Long <tyler4long@gmail.com>
2019-01-15 16:24:16 +0800

    Refactor code

--- RingCentral/RingCentral.cs
+++ RingCentral/RingCentral.cs
@@ -9,7 +9,7 @@ using Newtonsoft.Json.Linq;
 
 namespace RingCentral.Net
 {
-    public class RingCentral : IDisposable
+    public partial class RingCentral : IDisposable
     {
         public const string SANDBOX_HTTPS_SERVER = "https://platform.devtest.ringcentral.com";
         public const string PRODUCTION_HTTPS_SERVER = "https://platform.ringcentral.com";
@@ -81,55 +81,6 @@ namespace RingCentral.Net
             });
             this.wsClient.Send(requestBody);
             return t.Task;
-        }
-        public async Task<Response<T>> Request<T>(string method, string path, string body = null, bool basicAuth = false)
-        {
-            return (await Request(method, path, body, basicAuth)).ConvertTo<T>();
-        }
-
-        public Task<Response> Post(string path, Serializable body = null)
-        {
-            return Request("POST", path, body == null ? null : body.ToJsonString());
-        }
-        public async Task<Response<T>> Post<T>(string path, Serializable body = null)
-        {
-            return (await Post(path, body)).ConvertTo<T>();
-        }
-
-        public Task<Response> Put(string path, Serializable body = null)
-        {
-            return Request("PUT", path, body == null ? null : body.ToJsonString());
-        }
-        public async Task<Response<T>> Put<T>(string path, Serializable body = null)
-        {
-            return (await Put(path, body)).ConvertTo<T>();
-        }
-
-        public Task<Response> Delete(string path)
-        {
-            return Request("DELETE", path);
-        }
-        public async Task<Response<T>> Delete<T>(string path)
-        {
-            return (await Delete(path)).ConvertTo<T>();
-        }
-
-        public Task<Response> Get(string path)
-        {
-            return Request("GET", path);
-        }
-        public async Task<Response<T>> Get<T>(string path)
-        {
-            return (await Get(path)).ConvertTo<T>();
-        }
-
-        public Task<Response> Patch(string path)
-        {
-            return Request("PATCH", path);
-        }
-        public async Task<Response<T>> Patch<T>(string path)
-        {
-            return (await Patch(path)).ConvertTo<T>();
         }
 
         public async Task<Response<TokenInfo>> Authorize(string username, string extension, string password)
