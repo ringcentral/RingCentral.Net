5f9d6b2bc3bbc5706e704d82bfe733db104d6ed9
Tyler Long <tyler4long@gmail.com>
2019-02-10 22:35:13 +0800

    Refactor code

--- code-generator/paths.js
+++ code-generator/paths.js
@@ -14,80 +14,84 @@ const normalizedPaths = paths.map(p => p
   .replace(/\/\.search/, '/dotSearch')
 )
 
-const firstLevels = R.pipe(
-  R.map(p => p.split('/').filter(t => !R.isEmpty(t))[0]),
-  R.uniq
-)(paths)
-console.log(firstLevels)
+const generate = (prefix = '/') => {
+  const firstLevels = R.pipe(
+    R.map(p => p.split('/').filter(t => !R.isEmpty(t))[0]),
+    R.uniq
+  )(paths)
+  console.log(firstLevels)
 
-R.forEach(name => {
-  const folderName = changeCase.pascalCase(name)
-  const folderPath = path.join(outputDir, folderName)
-  fs.mkdirSync(folderPath)
-  var paramName = R.pipe(
-    R.filter(p => p.startsWith(`/${name}/{`)),
-    R.uniqBy(p => R.take(2, p.split('/').filter(t => t !== '')).join('/')),
-    R.map(p => R.init(R.tail(p.split('/')[2]))),
-    R.head
-  )(normalizedPaths)
-  console.log(paramName)
-  let defaultParamValue
-  if (name === 'restapi' && paramName === 'apiVersion') {
-    defaultParamValue = 'v1.0'
-  } else if (name === 'scim' && paramName === 'version') {
-    defaultParamValue = 'v2'
-  } else if (name === 'account' && paramName === 'accountId') {
-    defaultParamValue = '~'
-  } else if (name === 'extension' && paramName === 'extensionId') {
-    defaultParamValue = '~'
-  }
+  R.forEach(name => {
+    const folderName = changeCase.pascalCase(name)
+    const folderPath = path.join(outputDir, folderName)
+    fs.mkdirSync(folderPath)
+    var paramName = R.pipe(
+      R.filter(p => p.startsWith(`/${name}/{`)),
+      R.uniqBy(p => R.take(2, p.split('/').filter(t => t !== '')).join('/')),
+      R.map(p => R.init(R.tail(p.split('/')[2]))),
+      R.head
+    )(normalizedPaths)
+    console.log(paramName)
+    let defaultParamValue
+    if (name === 'restapi' && paramName === 'apiVersion') {
+      defaultParamValue = 'v1.0'
+    } else if (name === 'scim' && paramName === 'version') {
+      defaultParamValue = 'v2'
+    } else if (name === 'account' && paramName === 'accountId') {
+      defaultParamValue = '~'
+    } else if (name === 'extension' && paramName === 'extensionId') {
+      defaultParamValue = '~'
+    }
 
-  let code = `namespace RingCentral.Paths.${folderName}
+    let code = `namespace RingCentral.Paths.${folderName}
 {
     public class Index
     {
-      public RestClient rc;`
+        public RestClient rc;`
 
-  if (paramName) {
-    code += `
-      public string ${paramName};`
-  }
+    if (paramName) {
+      code += `
+        public string ${paramName};`
+    }
 
-  if (paramName) {
-    code += `
-      public Index(RestClient rc, string ${paramName} = ${defaultParamValue ? `"${defaultParamValue}"` : null})
-      {
-          this.rc = rc;
-          this.${paramName} = ${paramName};
-      }`
-  } else {
-    code += `
-      public Index(RestClient rc)
-      {
-          this.rc = rc;
-      }`
-  }
+    if (paramName) {
+      code += `
+        public Index(RestClient rc, string ${paramName} = ${defaultParamValue ? `"${defaultParamValue}"` : null})
+        {
+            this.rc = rc;
+            this.${paramName} = ${paramName};
+        }`
+    } else {
+      code += `
+        public Index(RestClient rc)
+        {
+            this.rc = rc;
+        }`
+    }
 
-  if (paramName) {
-    code += `
-      public string Path()
-      {
-          if (${paramName} != null)
-          {
-              return $"/${name}/{${paramName}}";
-          }
-          return "/${name}";
-      }`
-  } else {
-    code += `
-    public string Path()
-    {
-        return "/${name}";
-    }`
-  }
+    if (paramName) {
+      code += `
+        public string Path()
+        {
+            if (${paramName} != null)
+            {
+                return $"/${name}/{${paramName}}";
+            }
+            return "/${name}";
+        }`
+    } else {
+      code += `
+        public string Path()
+        {
+            return "/${name}";
+        }`
+    }
 
-  code += `
+    code += `
     }
 }`
-  fs.writeFileSync(path.join(folderPath, 'Index.cs'), code)
-})(firstLevels)
+    fs.writeFileSync(path.join(folderPath, 'Index.cs'), code)
+  })(firstLevels)
+}
+
+generate()
