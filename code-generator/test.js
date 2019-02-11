a3e33db7eba66833f1372c6037f93a98303b7613
Tyler Long <tyler4long@gmail.com>
2019-02-11 22:23:17 +0800

    Test method paramName

--- code-generator/test.js
+++ code-generator/test.js
@@ -1,12 +1,25 @@
+import * as R from 'ramda'
 
-const fl = 'restapi'
-const regexp = new RegExp(`/${fl}/(\\{[A-Za-z]+\\}|v[0-9.]+)`)
+// const fl = 'restapi'
+// const regexp = new RegExp(`/${fl}/(\\{[A-Za-z]+\\}|v[0-9.]+)`)
 
-let m = '/restapi/v1.0/account/{accountId}/meeting-recordings'.match(regexp)
-console.log(m[1])
+// let m = '/restapi/v1.0/account/{accountId}/meeting-recordings'.match(regexp)
+// console.log(m[1])
 
-m = '/restapi/{restapiVersion}'.match(regexp)
-console.log(m[1])
+// m = '/restapi/{restapiVersion}'.match(regexp)
+// console.log(m[1])
 
-m = '/restapi/fdasfads'.match(regexp)
-console.log(m === null)
+// m = '/restapi/fdasfads'.match(regexp)
+// console.log(m === null)
+
+const prefix = '/scim/{version}/'
+const name = 'Users'
+const paramName = R.pipe(
+  R.filter(p => p.startsWith(`${prefix}${name}/{`)),
+  R.map(p => p.substring(`${prefix}${name}/`.length)),
+  R.map(p => p.split('/').filter(t => t !== '')[0]),
+  R.map(t => t.substring(1, t.length - 1)),
+  R.head
+)(['/scim/{version}/Users/{id}'])
+
+console.log(paramName)
