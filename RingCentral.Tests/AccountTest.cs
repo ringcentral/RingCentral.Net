20ae03392f3d44727d10068d169a0859633c2e1c
Tyler Long <tyler4long@gmail.com>
2019-02-12 20:09:25 +0800

    Add AccountTest

---
+++ RingCentral.Tests/AccountTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RingCentral.Tests
+{
+    public class AccountTest
+    {
+        [Fact]
+        public async Task GetAccount()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+                
+                var account = await rc.Restapi().Account().Get();
+                Assert.NotNull(account);
+                Assert.NotNull(account.serviceInfo);
+            }
+        }
+    }
+}
