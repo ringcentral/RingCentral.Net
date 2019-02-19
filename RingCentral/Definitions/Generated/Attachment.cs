17f824032f80620e7cd7e1e65d75dc76985f4e51
Tyler Long <tyler4long@gmail.com>
2019-02-19 14:32:07 +0800

    Generate Attachment.cs

---
+++ RingCentral/Definitions/Generated/Attachment.cs
@@ -0,0 +1,11 @@
+namespace RingCentral
+{
+    public class Attachment
+    {
+        // File name with extension, such as "example.png"
+        public string fileName;
+
+        // Binary content of the file
+        public byte[] bytes;
+    }
+}
