7b971df14c98e878d99bb3c5e6a5c25783d08761
Tyler Long <tyler4long@gmail.com>
2019-02-21 21:21:01 +0800

    Add ServiceInfoTest

---
+++ RingCentral.Tests/ServiceInfoTest.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace RingCentral.Tests
+{
+    public class ServiceInfoTest
+    {
+        [Fact]
+        public async void ServiceInfo()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+                
+                // account service-info
+                var account = rc.Restapi().Account();
+                var accountServiceInfo = await account.ServiceInfo().Get();
+                var faxReceiving = accountServiceInfo.serviceFeatures.First(item => item.featureName == "FaxReceiving")
+                    .enabled;
+                Assert.True(faxReceiving);
+
+                // meeting service-info
+                var meetingServiceInfo = await account.Extension().Meeting().ServiceInfo().Get();
+                Assert.NotNull(meetingServiceInfo);
+                Assert.StartsWith("http", meetingServiceInfo.uri);
+            }
+        }
+    }
+}
