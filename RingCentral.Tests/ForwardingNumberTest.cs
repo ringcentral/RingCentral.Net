792bacceda2f38e7be81014d7b399773a9162141
Tyler Long <tyler4long@gmail.com>
2019-02-21 16:21:42 +0800

    Add ForwardingNumberTest

---
+++ RingCentral.Tests/ForwardingNumberTest.cs
@@ -0,0 +1,34 @@
+using System;
+using Xunit;
+
+namespace RingCentral.Tests
+{
+    public class ForwardingNumberTest
+    {
+        [Fact]
+        public async void GetForwardingNumberList()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+                var extension = rc.Restapi().Account().Extension();
+                var response = await rc.Get(extension.ForwardingNumber().Path(false));
+                var body = await response.Content.ReadAsStringAsync();
+                Assert.NotNull(response);
+                Assert.NotEmpty(body);
+
+                var r = await extension.ForwardingNumber().List();
+                Assert.NotEmpty(r.records);
+            }
+        }
+
+    }
+}
