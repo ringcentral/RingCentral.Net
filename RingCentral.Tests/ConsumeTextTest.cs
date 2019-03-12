16fe3200e4457d73da24e64064fdb391d79b1a77
Tyler Long <tyler4long@gmail.com>
2019-03-12 17:15:48 +0800

    Add test case for UpdateGlipPostText

---
+++ RingCentral.Tests/ConsumeTextTest.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+
+namespace RingCentral.Tests
+{
+    public class ConsumeTextTest
+    {
+        [Fact]
+        public async void UpdateGlipPostText()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+                var groupId = (await rc.Restapi().Glip().Groups().List()).records[0].id;
+                var postId = (await rc.Restapi().Glip().Groups(groupId).Posts().Get()).records[0].id;
+                var newText = Guid.NewGuid().ToString();
+                var r = await rc.Restapi().Glip().Groups(groupId).Posts(postId).Text().Put(newText);
+                Assert.Equal(r, newText);
+            }
+        }
+    }
+}
