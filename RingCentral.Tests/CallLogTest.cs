36299a85512fa35bc737e1beb78b7a6a810dd6fd
Tyler Long <tyler4long@gmail.com>
2019-03-21 09:37:07 +0800

    Add test for call log FilterByPhoneNumber

--- RingCentral.Tests/CallLogTest.cs
+++ RingCentral.Tests/CallLogTest.cs
@@ -28,5 +28,46 @@ namespace RingCentral.Tests
                 Assert.Equal(3, callLogResponse.records.Length);
             }
         }
+
+        [Fact]
+        public async void FilterByPhoneNumber()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+
+                var callLogResponse = await rc.Restapi().Account().Extension().CallLog().List(new LoadUserCallLogParameters
+                {
+                    perPage = 1,
+                    dateFrom = DateTime.UtcNow.AddMonths(-6).ToString("o")
+                });
+                Assert.Equal(1, callLogResponse.records.Length);
+
+                var fromNumber = callLogResponse.records[0].@from.phoneNumber;
+                callLogResponse = await rc.Restapi().Account().Extension().CallLog().List(new LoadUserCallLogParameters
+                {
+                    perPage = 1,
+                    dateFrom = DateTime.UtcNow.AddMonths(-6).ToString("o"),
+                    phoneNumber = fromNumber  // +123456789
+                });
+                Assert.Equal(0, callLogResponse.records.Length);
+                
+                callLogResponse = await rc.Restapi().Account().Extension().CallLog().List(new LoadUserCallLogParameters
+                {
+                    perPage = 1,
+                    dateFrom = DateTime.UtcNow.AddMonths(-6).ToString("o"),
+                    phoneNumber = fromNumber.TrimStart('+') // 123456789
+                });
+                Assert.Equal(1, callLogResponse.records.Length);
+            }
+        }
     }
 }
