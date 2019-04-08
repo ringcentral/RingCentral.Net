8fea80ed561ad19f71c505e03261c3b8b9bbd749
Tyler Long <tyler4long@gmail.com>
2019-04-08 10:11:16 +0800

    Add RingCentral.Tests/GlipChatTest.cs

---
+++ RingCentral.Tests/GlipChatTest.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+
+namespace RingCentral.Tests
+{
+    public class GlipChatTest
+    {
+        [Fact]
+        public async void GetEveryOneTeam()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+                var result = await rc.Restapi().Glip().Chats().List(new ListGlipChatsParameters{ type = new [] { "Everyone" } });
+                Assert.Single(result.records);
+            }
+        }
+    }
+}
