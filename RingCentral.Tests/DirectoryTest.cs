d9b91da571ccaa3d81506a361380475bd24ac3fe
Tyler Long <tyler4long@gmail.com>
2019-04-22 17:21:49 +0800

    Add DirectoryTest.cs

---
+++ RingCentral.Tests/DirectoryTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace RingCentral.Tests
+{
+    public class DirectoryTest
+    {
+        [Fact]
+        public async void FetchDirectory()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+
+                var directoryResource = await rc.Restapi().Account().Directory().Entries().List();
+                var sites = directoryResource.records.Select(r => r.site).ToArray();
+            }
+        }
+    }
+}
