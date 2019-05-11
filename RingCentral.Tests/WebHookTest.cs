281683179d1c19d9ca3ef8fa42835a2ad0319fb0
Tyler Long <tyler4long@gmail.com>
2019-05-10 17:12:45 -0700

    Add VerificationToken, comment out

--- RingCentral.Tests/WebHookTest.cs
+++ RingCentral.Tests/WebHookTest.cs
@@ -37,5 +37,34 @@ namespace RingCentral.Tests
                 }
             }
         }
+
+//        [Fact]
+//        public async void VerificationToken()
+//        {
+//            using (var rc = new RestClient(
+//                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+//                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+//                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+//            ))
+//            {
+//                await rc.Authorize(
+//                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+//                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+//                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+//                );
+//                var subscriptionInfo = await rc.Restapi().Subscription().Post(new CreateSubscriptionRequest
+//                {
+//                    eventFilters = new[] {"/restapi/v1.0/account/~/extension/~/presence?detailedTelephonyState=true"},
+//                    deliveryMode = new NotificationDeliveryModeRequest
+//                    {
+//                        transportType = "WebHook",
+//                        address = "https://75ef5993.ngrok.io/webhook",
+//                        verificationToken = "hello-world"
+//                    },
+//                    expiresIn = 630720000
+//                });
+//                Assert.Equal(630720000, subscriptionInfo.expiresIn);
+//            }
+//        }
     }
 }
