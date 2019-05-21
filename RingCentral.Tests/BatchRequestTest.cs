e2a34710b7038e9e22390b670c3a540c4a0baf46
Tyler Long <tyler4long@gmail.com>
2019-05-21 14:18:59 -0700

    Add two new tests

---
+++ RingCentral.Tests/BatchRequestTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using Xunit;
+
+namespace RingCentral.Tests
+{
+    public class BatchRequestTest
+    {
+        [Fact]
+        public async void AfterCall()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+                var contactList = await rc.Restapi().Account().Extension().AddressBook().Contact().List();
+                var ids = contactList.records.Select(c => c.id).Take(3);
+                var str = await rc.Get<string>(rc.Restapi().Account().Extension().AddressBook().Contact(string.Join(",", ids)).Path());
+            }
+        }
+    }
+}
---
+++ RingCentral.Tests/PermissionsTest.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace RingCentral.Tests
+{
+    public class PermissionsTest
+    {
+        [Fact]
+        public async void GetPermissions()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+
+                var str = await rc.Get<string>("/restapi/v1.0/dictionary/permission/HUD");
+                Assert.Contains("HUD", str);
+            }
+        }
+    }
+}
