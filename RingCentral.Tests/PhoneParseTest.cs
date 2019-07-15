885c37bca014d578bb8ef91b9f0c524b6d03530b
Tyler Long <tyler4long@gmail.com>
2019-07-15 09:48:12 -0700

    Add PhoneParseTest

---
+++ RingCentral.Tests/PhoneParseTest.cs
@@ -0,0 +1,31 @@
+using System;
+using Xunit;
+
+namespace RingCentral.Tests
+{
+    public class PhoneParseTest
+    {
+        [Fact]
+        public async void ParseNumber()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+
+                var r = await rc.Restapi().NumberParser().Parse().Post(new ParsePhoneNumberRequest
+                {
+                    originalStrings = new []{ "+1650-666-8888", "+1650-666-9999" }
+                });
+                Assert.NotNull(r);
+            }
+        }
+    }
+}
