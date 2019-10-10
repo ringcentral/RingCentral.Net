640191cf4aacccad31a8d925de64e7c08d39538a
Tyler Long <tyler4long@gmail.com>
2019-10-09 17:32:44 -0700

    Release 2.1.0

--- RingCentral.Net/RestClient.cs
+++ RingCentral.Net/RestClient.cs
@@ -44,7 +44,7 @@ namespace RingCentral
         public async Task<HttpResponseMessage> Request(HttpRequestMessage httpRequestMessage)
         {
             var httpClient = new HttpClient();
-            httpRequestMessage.Headers.Add("X-User-Agent", $"{appName}/{appVersion} RingCentral.Net/2.0.2");
+            httpRequestMessage.Headers.Add("X-User-Agent", $"{appName}/{appVersion} RingCentral.Net/2.1.0");
             httpRequestMessage.Headers.Authorization = token == null
                 ? new AuthenticationHeaderValue("Basic",
                     Convert.ToBase64String(
