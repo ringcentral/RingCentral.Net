de72881d6d4e8f459ccf725f1670106d33346cbe
Tyler Long <tyler4long@gmail.com>
2020-04-22 17:20:14 -0700

    Add test case about low level api GetExtensionInfo

--- RingCentral.Tests/LowLevelApiTest.cs
+++ RingCentral.Tests/LowLevelApiTest.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace RingCentral.Tests
@@ -37,6 +39,35 @@ namespace RingCentral.Tests
                 // non-generic version
                 var responseMessage2 = await rc.Get("/restapi/v1.0/dictionary/country/46");
                 Assert.Equal(HttpStatusCode.OK, responseMessage2.StatusCode);
+            }
+        }
+
+        [Fact]
+        public async void GetExtensionInfo()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+                // normal way to get extension info
+                var extInfo = await rc.Restapi().Account("~").Extension("~").Get();
+                Assert.NotNull(extInfo.id);
+
+                // low level way to get extension info + headers info
+                var httpResponseMessage = await rc.Get(rc.Restapi().Account("~").Extension("~").Path(true));
+                var headers = httpResponseMessage.Headers;
+                Assert.NotNull(headers);
+                var responseBodyStr = await httpResponseMessage.Content.ReadAsStringAsync();
+                Assert.NotNull(responseBodyStr);
+                extInfo = JsonConvert.DeserializeObject<GetExtensionInfoResponse>(responseBodyStr);
+                Assert.NotNull(extInfo.id);
             }
         }
     }
