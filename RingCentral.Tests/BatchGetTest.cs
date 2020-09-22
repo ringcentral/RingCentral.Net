7635cc8e4d34639dab4751f9ddc333f1d47a86cd
Tyler Long <tyler4long@gmail.com>
2020-09-21 17:20:20 -0700

    Try to fix CI

--- RingCentral.Tests/BatchGetTest.cs
+++ RingCentral.Tests/BatchGetTest.cs
@@ -65,7 +65,7 @@ namespace RingCentral.Tests
                 );
 
                 var callLogsResponse = await rc.Restapi().Account().Extension().CallLog()
-                    .List(new ReadUserCallLogParameters {perPage = 3, dateFrom = DateTime.Today.AddYears(-1).ToString("O")});
+                    .List(new ReadUserCallLogParameters {perPage = 3, dateFrom = DateTime.UtcNow.AddYears(-1).ToString("O")});
                 var callLogIds = string.Join(',', callLogsResponse.records.Select(r => r.id));
 
                 var batchResponses = await rc.Restapi().Account().Extension().CallLog(callLogIds).BatchGet();
@@ -94,7 +94,7 @@ namespace RingCentral.Tests
                 );
 
                 var callLogsResponse = await rc.Restapi().Account().Extension().CallLog()
-                    .List(new ReadUserCallLogParameters {perPage = 3, dateFrom = DateTime.Today.AddYears(-3).ToString("O")});
+                    .List(new ReadUserCallLogParameters {perPage = 3, dateFrom = DateTime.UtcNow.AddYears(-3).ToString("O")});
                 var callLogIds = string.Join(',', callLogsResponse.records.Select(r => r.id));
 
                 var batchResponses =
@@ -126,9 +126,8 @@ namespace RingCentral.Tests
                 ArgumentException argumentException = null;
                 try
                 {
-                    var batchResponses =
-                        await rc.BatchGet<UserCallLogRecord>(rc.Restapi().Account().Extension().CallLog(callLogIds)
-                            .Path());
+                    await rc.BatchGet<UserCallLogRecord>(rc.Restapi().Account().Extension().CallLog(callLogIds)
+                        .Path());
                 }
                 catch (ArgumentException ae)
                 {
