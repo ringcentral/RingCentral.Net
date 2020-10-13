88be5a8af548d6d00519fb96832cf27bc31c7d82
Tyler Long <tyler4long@gmail.com>
2020-10-12 20:55:34 -0700

    Add test for retry extension

---
+++ RingCentral.Tests/Extensions/RetryTest.cs
@@ -0,0 +1,32 @@
+using System;
+using RingCentral.Net.Retry;
+using Xunit;
+
+namespace RingCentral.Tests.Extensions
+{
+    public class RetryTest
+    {
+        [Fact]
+        public async void EnableAutoRetry()
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                await rc.Authorize(
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                    Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                );
+                var retryExtension = new RetryExtension();
+                rc.InstallExtension(retryExtension);
+                retryExtension.enabled = false; // remove this line to enable auto retry
+                var extInfo = await rc.Restapi().Account().Extension().Get();
+                Assert.NotNull(extInfo);
+                Assert.NotNull(extInfo.id);
+            }
+        }
+    }
+}
