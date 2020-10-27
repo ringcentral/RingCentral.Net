c385008a84bd9f8d9fd147264cd8bc4309f2230f
Tyler Long <tyler4long@gmail.com>
2020-10-26 17:04:36 -0700

    Minor change to code format

--- RingCentral.Net/RestRequestConfig.cs
+++ RingCentral.Net/RestRequestConfig.cs
@@ -5,7 +5,7 @@ namespace RingCentral
     public class RestRequestConfig
     {
         public static readonly RestRequestConfig DefaultInstance = new RestRequestConfig();
-        
+
         // So that you can cancel an HTTP request
         public CancellationToken cancellationToken = CancellationToken.None;
 
