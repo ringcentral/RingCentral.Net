fe2e99b15782d61e606aebce74c398bc872221f4
Tyler Long <tyler4long@gmail.com>
2020-11-02 11:18:54 -0800

    AuthorizeUri extension supports PKCE

--- RingCentral.Net.AuthorizeUri/AuthorizeUriExtension.cs
+++ RingCentral.Net.AuthorizeUri/AuthorizeUriExtension.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace RingCentral.Net.AuthorizeUri
 {
     public class AuthorizeUriExtension : SdkExtension
     {
-        private RestClient rc;
+        private RestClient _rc;
+        public string codeVerifier;
 
         public override void Install(RestClient rc)
         {
-            this.rc = rc;
+            this._rc = rc;
         }
 
-        // todo: support PKCE
         public string BuildUri(AuthorizeRequest authorizeRequest)
         {
             if (authorizeRequest.response_type == null)
@@ -22,10 +24,26 @@ namespace RingCentral.Net.AuthorizeUri
 
             if (authorizeRequest.client_id == null)
             {
-                authorizeRequest.client_id = this.rc.clientId;
+                authorizeRequest.client_id = this._rc.clientId;
             }
 
-            var uriBuilder = new UriBuilder(this.rc.server)
+            // PKCE: https://medium.com/ringcentral-developers/use-authorization-code-pkce-for-ringcentral-api-in-client-app-e9108f04b5f0
+            if (authorizeRequest.code_challenge_method == "S256")
+            {
+                var randomBytes = new Byte[32];
+                new Random().NextBytes(randomBytes);
+                this.codeVerifier = Convert.ToBase64String(randomBytes)
+                    .Replace("+", "-")
+                    .Replace("/", "_")
+                    .Replace("=", "");
+                var sha256Hash = new SHA256Managed().ComputeHash(Encoding.UTF8.GetBytes(this.codeVerifier));
+                authorizeRequest.code_challenge = Convert.ToBase64String(sha256Hash)
+                    .Replace("+", "-")
+                    .Replace("/", "_")
+                    .Replace("=", "");
+            }
+
+            var uriBuilder = new UriBuilder(this._rc.server)
             {
                 Path = "/restapi/oauth/authorize",
                 Query = string.Join("&",
