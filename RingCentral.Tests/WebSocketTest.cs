0aded9cd8b7689a50d076eefbcb3476d1c7b4016
Tyler Long <tyler4long@gmail.com>
2020-11-13 11:31:21 -0800

    Release 5.0.0-beta2

--- RingCentral.Tests/WebSocketTest.cs
+++ RingCentral.Tests/WebSocketTest.cs
@@ -37,27 +37,27 @@ namespace RingCentral.Tests
                     Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
                     Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
                 );
-                var webSocketExtension = new WebSocketExtension(new WebSocketOptions{debugMode = true});
-                Console.SetOut(new MyTextWriter(_testOutputHelper));
-                await rc.InstallExtension(webSocketExtension);
-                var autoRefreshExtension = new AutoRefreshExtension();
-                autoRefreshExtension.Start();
-                await rc.InstallExtension(autoRefreshExtension);
-                var eventFilters = new[] {"/restapi/v1.0/account/~/extension/~/message-store"};
-                await webSocketExtension.Subscribe(eventFilters, message =>
-                {
-                    _testOutputHelper.WriteLine(message);
-                });
-                while (true)
-                {
-                    await rc.Restapi().Account().Extension().CompanyPager().Post(new CreateInternalTextMessageRequest
-                    {
-                        from = new PagerCallerInfoRequest {extensionNumber = "101"},
-                        to = new[] {new PagerCallerInfoRequest {extensionNumber = "101"}},
-                        text = "Hello world",
-                    });
-                    await Task.Delay(120000);
-                }
+                // var webSocketExtension = new WebSocketExtension(new WebSocketOptions{debugMode = true});
+                // Console.SetOut(new MyTextWriter(_testOutputHelper));
+                // await rc.InstallExtension(webSocketExtension);
+                // var autoRefreshExtension = new AutoRefreshExtension();
+                // autoRefreshExtension.Start();
+                // await rc.InstallExtension(autoRefreshExtension);
+                // var eventFilters = new[] {"/restapi/v1.0/account/~/extension/~/message-store"};
+                // await webSocketExtension.Subscribe(eventFilters, message =>
+                // {
+                //     _testOutputHelper.WriteLine(message);
+                // });
+                // while (true)
+                // {
+                //     await rc.Restapi().Account().Extension().CompanyPager().Post(new CreateInternalTextMessageRequest
+                //     {
+                //         from = new PagerCallerInfoRequest {extensionNumber = "101"},
+                //         to = new[] {new PagerCallerInfoRequest {extensionNumber = "101"}},
+                //         text = "Hello world",
+                //     });
+                //     await Task.Delay(120000);
+                // }
             }
         }
     }
