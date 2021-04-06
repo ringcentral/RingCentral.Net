ce1267bd965c9058af4d9bbfba241f654b942b1a
Tyler Long <tyler4long@gmail.com>
2021-04-06 11:42:31 -0700

    Generate paths

--- code-generator/src/paths.ts
+++ code-generator/src/paths.ts
@@ -1,9 +1,57 @@
 import {parsed} from 'ringcentral-open-api-parser';
 import fs from 'fs';
 import path from 'path';
+import {pascalCase} from 'change-case';
+import R from 'ramda';
 
 const outputDir = '../RingCentral.Net/Paths';
 
 for (const item of parsed.paths) {
   console.log(item);
+  const itemPaths = item.paths.map(p => pascalCase(p));
+  const parentPaths = R.init(itemPaths);
+  const code = `
+using System.Threading.Tasks;
+
+namespace RingCentral.Paths.${itemPaths.join('.')}
+{
+    public partial class Index
+    {
+        public RestClient rc;
+        public ${parentPaths.join('.')}.Index parent;
+        ${item.parameter ? `public string ${item.parameter};` : ''}
+
+        public Index(${parentPaths.join('.')}.Index parent${
+    item.parameter ? `, string ${item.parameter} = null` : ''
+  })
+        {
+            this.parent = parent;
+            this.rc = parent.rc;
+            ${
+              item.parameter
+                ? `this.${item.parameter} = ${item.parameter};`
+                : ''
+            }
+        }
+    }
+}
+
+namespace RingCentral.Paths.${parentPaths.join('.')}
+{
+    public partial class Index
+    {
+        public ${itemPaths.join('.')}.Index ${R.last(itemPaths)}(${
+    item.parameter ? `string ${item.parameter} = null` : ''
+  })
+        {
+            return new ${itemPaths.join('.')}.Index(this${
+    item.parameter ? `, ${item.parameter}` : ''
+  });
+        }
+    }
+}
+`.trim();
+  const folder = path.join(outputDir, ...itemPaths);
+  fs.mkdirSync(folder, {recursive: true});
+  fs.writeFileSync(path.join(folder, 'Index.cs'), code);
 }
