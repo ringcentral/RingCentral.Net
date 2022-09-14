7d5b4d53216c83e4db7a1b60c3f53286b3a1714f
Tyler Long <tyler4long@gmail.com>
2022-09-14 11:37:57 -0700

    Fix resend HttpRequestMessage issue

--- RingCentral.Net.Retry/RetryExtension.cs
+++ RingCentral.Net.Retry/RetryExtension.cs
@@ -1,7 +1,49 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace RingCentral.Net.Retry
 {
+    // Ref: https://stackoverflow.com/a/46026230/862862
+    public static class Utils
+    {
+        public static async Task<HttpRequestMessage> CloneAsync(this HttpRequestMessage request)
+        {
+            var clone = new HttpRequestMessage(request.Method, request.RequestUri)
+            {
+                Content = await request.Content.CloneAsync().ConfigureAwait(false),
+                Version = request.Version
+            };
+            foreach (KeyValuePair<string, object> prop in request.Properties)
+            {
+                clone.Properties.Add(prop);
+            }
+            foreach (KeyValuePair<string, IEnumerable<string>> header in request.Headers)
+            {
+                clone.Headers.TryAddWithoutValidation(header.Key, header.Value);
+            }
+
+            return clone;
+        }
+
+        public static async Task<HttpContent> CloneAsync(this HttpContent content)
+        {
+            if (content == null) return null;
+
+            var ms = new MemoryStream();
+            await content.CopyToAsync(ms).ConfigureAwait(false);
+            ms.Position = 0;
+
+            var clone = new StreamContent(ms);
+            foreach (KeyValuePair<string, IEnumerable<string>> header in content.Headers)
+            {
+                clone.Headers.Add(header.Key, header.Value);
+            }
+            return clone;
+        }
+    }
+    
     public class RetryExtension : SdkExtension
     {
         private readonly RetryOptions _options;
@@ -18,7 +60,7 @@ namespace RingCentral.Net.Retry
             {
                 try
                 {
-                    return await extensibleRequest(httpRequestMessage, restRequestConfig);
+                    return await extensibleRequest(await httpRequestMessage.CloneAsync(), restRequestConfig);
                 }
                 catch (RestException re)
                 {
