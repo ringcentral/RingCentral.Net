2a36d6ad817d7ea72676a59b5f8f2d1e3556c761
Tyler Liu <tyler4long@gmail.com>
2024-03-05 14:49:51 -0800

    Release 6.1.3

--- RingCentral.Net/RestClient.cs
+++ RingCentral.Net/RestClient.cs
@@ -74,7 +74,7 @@ namespace RingCentral
         {
             restRequestConfig = restRequestConfig ?? RestRequestConfig.DefaultInstance;
 
-            httpRequestMessage.Headers.Add("X-User-Agent", $"{appName}/{appVersion} RingCentral.Net/6.1.2");
+            httpRequestMessage.Headers.Add("X-User-Agent", $"{appName}/{appVersion} RingCentral.Net/6.1.3");
             httpRequestMessage.Headers.Authorization =
                 BasicAuthPaths.Contains(httpRequestMessage.RequestUri.AbsolutePath)
                     ? new AuthenticationHeaderValue("Basic",
