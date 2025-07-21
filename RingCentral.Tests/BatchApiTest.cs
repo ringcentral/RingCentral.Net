f2f6058d270d7fbcc3da405ec881c3fe7e82f517
Tyler Liu <tyler4long@gmail.com>
2025-07-21 14:46:05 -0700

    Finish batch API test.

--- RingCentral.Tests/BatchApiTest.cs
+++ RingCentral.Tests/BatchApiTest.cs
@@ -1,7 +1,16 @@
 using System.Linq;
+using Newtonsoft.Json;
 using Xunit;
 
 namespace RingCentral.Tests;
+
+// This is a class to deserialize the batch result
+public class MessageStoreBatchResponse
+{
+    public string resourceId { get; set; }
+    public int status { get; set; }
+    public GetMessageInfoResponse body { get; set; }
+}
 
 [Collection("Sequential")]
 public class BatchApiTest
@@ -24,6 +33,13 @@ public class BatchApiTest
             restRequestConfig.customHeaders.Add("Accept", "application/vnd.ringcentral.multipart+json");
             var result = await rc.Get<string>(endpoint, null, restRequestConfig);
             Assert.NotNull(result);
+            
+            // parsing
+            var parsed = JsonConvert.DeserializeObject<MessageStoreBatchResponse[]>(result);
+            Assert.NotNull(parsed);
+            Assert.Equal(2, parsed.Length);
+            Assert.NotNull(parsed[0].body);
+            Assert.NotNull(parsed[1].body.id);
         }
     }
 }
