970720efe4e31552b32da2c9750ed51e9abc4c2b
Tyler Liu <tyler4long@gmail.com>
2025-09-23 15:44:58 -0700

    Release 6.4.2

--- RingCentral.Net/RestClient.cs
+++ RingCentral.Net/RestClient.cs
@@ -69,7 +69,7 @@ namespace RingCentral
         {
             restRequestConfig = restRequestConfig ?? RestRequestConfig.DefaultInstance;
 
-            httpRequestMessage.Headers.Add("X-User-Agent", $"{appName}/{appVersion} RingCentral.Net/6.4.1");
+            httpRequestMessage.Headers.Add("X-User-Agent", $"{appName}/{appVersion} RingCentral.Net/6.4.2");
 
             // customer headers
             foreach (var h in restRequestConfig.customHeaders)
